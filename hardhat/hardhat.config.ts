c787d65f1f0b10818dca7dad5c88ab4cdae119ea
Kristian Hadzhikolev <kris@bull44.com>
2025-04-17 20:55:57 +0300

    changed account

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -9,9 +9,7 @@ const config: HardhatUserConfig = {
     devnet: {
       url: "http://localhost:8545",
       chainId: 1337,
-      accounts: [
-        "0x1000000000000000000000000000000000000000000000000000000000000001"
-      ]
+      accounts: ['0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d']
     },
   },
 };
