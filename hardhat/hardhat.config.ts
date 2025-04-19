70bf38e4d9e943153aecf7380ffe82e87a7ab3c0
Kristian Hadzhikolev <balccbf@gmail.com>
2025-04-19 16:23:42 +0300

    Added default account removed live property

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -9,8 +9,7 @@ const config: HardhatUserConfig = {
     devnet: {
       url: "http://localhost:8545",
       chainId: 1337,
-      accounts: ['0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d'],
-      live: false,
+      accounts: ['4f3edf983ac636a65a842ce7c78d9aa706d3b113b37c936e9c37fda7f1f7c8c2'],
     },
   },
 };
